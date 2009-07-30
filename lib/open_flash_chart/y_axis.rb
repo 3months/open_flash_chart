1613949fe712a4e9cb34a373f3e8aad27b0dd65f
Joe Mahoney <joe@joe.3months.com>
2009-07-30 14:38:30 +1200

    Add support for set_label_text

--- lib/open_flash_chart/y_axis.rb
+++ lib/open_flash_chart/y_axis.rb
@@ -1,3 +1,15 @@
 module OpenFlashChart
-  class YAxis < YAxisBase ; end
+  class YAxis < YAxisBase
+
+    #OpenFlashChart2 supports "magic variables" for formatting the
+    #text of the labels on the y-axis via the YAxis#set_label_text
+    #According to the example output
+    #(http://teethgrinder.co.uk/open-flash-chart-2/gallery/y-axis-format-2.php)
+    #The output of set_label_text should look like somthing like this:
+    #"labels": { "text": "$#val# million" }
+    def set_label_text(label_text)
+        @labels = { :text =>label_text }
+    end
+
+  end
 end
